2d6c4473fd22b820f7bd63435bbb76dede22a464
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-07-15 12:11:30 -0300

    api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,61 +1,63 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string,
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`)
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositorios no Github.</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository} >
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img 
-          src="https://avatars3.githubusercontent.com/u/31639140?s=460&u=c6139737c207d358607693edba59475b93cda879&v=4" 
-          alt="Francisco Pinto" />
-          <div>
-            <strong>
-              frapimoneto/goStack01
-            </strong>
-            <p>Exercícios realizados no módulo um do bootcamp goStack Rocketseat</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login} />
+            <div>
+              <strong>
+                {repository.full_name}
+              </strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img 
-          src="https://avatars3.githubusercontent.com/u/31639140?s=460&u=c6139737c207d358607693edba59475b93cda879&v=4" 
-          alt="Francisco Pinto" />
-          <div>
-            <strong>
-              frapimoneto/goStack01
-            </strong>
-            <p>Exercícios realizados no módulo um do bootcamp goStack Rocketseat</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img 
-          src="https://avatars3.githubusercontent.com/u/31639140?s=460&u=c6139737c207d358607693edba59475b93cda879&v=4" 
-          alt="Francisco Pinto" />
-          <div>
-            <strong>
-              frapimoneto/goStack01
-            </strong>
-            <p>Exercícios realizados no módulo um do bootcamp goStack Rocketseat</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   )
